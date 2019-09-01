54967a4ed2bc5088c830e02b63da78e72b60b9d1
zsx950110 <zsx_office@163.com>
2019-09-01 13:44:59 +0800

    添加ssodemo

--- myProject/src/main/java/person/controller/TestController.java
+++ myProject/src/main/java/person/controller/TestController.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.HandlerInterceptor;
 import person.pojo.Book;
 import person.pojo.Comment;
 import person.pojo.TestUser;
