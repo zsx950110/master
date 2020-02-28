96bd4abeb65e71433ea50372a4404c8c38f459f2
zsx950110 <zsx_office@163.com>
2020-02-28 14:30:32 +0800

    nio测试

--- myProject/src/main/java/person/NIOtest/NIOClient.java
+++ myProject/src/main/java/person/NIOtest/NIOClient.java
@@ -1,9 +1,57 @@
 package person.NIOtest;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.channels.SocketChannel;
+
 /**
  * @Author: zhangshaoxuan
  * @DateTime: 2020/2/27 17:36
  * @Description: nio的客户端
  */
 public class NIOClient {
+    public static void main(String[] args) {
+        String path =  "D:\\sofa_workspace-20181224.zip";
+      //  String path1 =  "F:\\技术文件\\qqq.txt";
+
+        FileInputStream inputStream = null;
+        Socket socket = null;
+        SocketChannel socketChannel = null;
+        FileChannel fileChannel = null;
+        try {
+            inputStream = new FileInputStream(path);
+          //   socket =  new Socket("127.0.0.1",9070);
+            ////获得通道
+            socketChannel  =SocketChannel.open(new InetSocketAddress("127.0.0.1",9010));
+            fileChannel = inputStream.getChannel();
+            //分配buffer
+            ByteBuffer byteBuffer = ByteBuffer.allocate(1024*1024);
+            //读写
+            System.out.println("-------------client开始读写-----------------");
+            while(fileChannel.read(byteBuffer)!=-1){
+                //切换读写模式
+                byteBuffer.flip();
+                System.out.println(byteBuffer.toString());
+                socketChannel.write(byteBuffer);
+                byteBuffer.clear();
+            }
+            System.out.println("----------client读写完成--------------------");
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+           try {
+                socketChannel.close();
+             //  socket.close();
+               fileChannel.close();
+              inputStream.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
--- myProject/src/main/java/person/NIOtest/NIOServer.java
+++ myProject/src/main/java/person/NIOtest/NIOServer.java
@@ -1,9 +1,10 @@
 package person.NIOtest;
 
-import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.net.InetSocketAddress;
 import java.net.ServerSocket;
+import java.net.Socket;
 import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
 import java.nio.channels.ServerSocketChannel;
@@ -18,25 +19,36 @@ public class NIOServer {
     public static void main(String[] args) {
         FileOutputStream fileOutputStream = null;
         FileChannel outChannel = null;
+        ServerSocketChannel serverSocketChannel = null;
         try {
-             fileOutputStream = new FileOutputStream("F:\\技术文件\\aaaaa.pdf");
-            ServerSocket serverSocket = new ServerSocket(9090);
-            //1获取通道
-            ServerSocketChannel serverSocketChannel = serverSocket.getChannel();
-             outChannel = fileOutputStream.getChannel();
-            //2.定义buffer
-            ByteBuffer byteBuffer = ByteBuffer.allocate(1024*1024);
-            SocketChannel accept = serverSocketChannel.accept();
-            //读
-            while (accept.read(byteBuffer)>-1){
-                //写
-                outChannel.write(byteBuffer);
+            //ctrl+alt+i 自动缩进
+            fileOutputStream = new FileOutputStream("F:\\技术文件\\aaaaa.zip");
+            outChannel = fileOutputStream.getChannel();
+            serverSocketChannel = ServerSocketChannel.open();
+            serverSocketChannel.bind(new InetSocketAddress(9010));
+            System.out.println("------------server开始监听9010-------------------");
+            while (true){
+                SocketChannel socketChannel = serverSocketChannel.accept();
+                //2.定义buffer
+                ByteBuffer byteBuffer = ByteBuffer.allocate(1024*1024);
+                //读
+                System.out.println("---------------server开始读写--------------");
+                while (socketChannel.read(byteBuffer)!=-1){
+                    //改变读写标志位position和limit，切换读写模式
+                    byteBuffer.flip();
+                    System.out.println("------在写中-----");
+                    //写
+                    System.out.println(byteBuffer.toString());
+                    outChannel.write(byteBuffer);
+                    byteBuffer.clear();
+                }
+                System.out.println("-------------server读写完成-----------------");
             }
-            ;
         } catch (IOException e) {
             e.printStackTrace();
         } finally {
             try {
+                serverSocketChannel.close();
                 outChannel.close();
                 fileOutputStream.close();
             } catch (IOException e) {
