95c3434be8afc1aaf925fae87c53f06bd1abdb0d
zsx950110 <zsx_office@163.com>
2020-06-30 17:45:54 +0800

    kafkademo提交

--- transferingB/src/main/java/person/zsx/transfering/Util/KafkaUtils.java
+++ transferingB/src/main/java/person/zsx/transfering/Util/KafkaUtils.java
@@ -4,6 +4,7 @@ import org.apache.kafka.clients.consumer.Consumer;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.kafka.common.serialization.StringSerializer;
 
@@ -36,6 +37,7 @@ public class KafkaUtils {
         if (kafkaConsumer==null){
             synchronized (KafkaUtils.class){
                 if (kafkaConsumer==null){
+
                     kafkaConsumer =   new KafkaConsumer(getConsumerProperties());
                 }
             }
@@ -50,14 +52,26 @@ public class KafkaUtils {
     public static Properties getConsumerProperties(){
         //属性
         Properties props = new Properties();
+        /**
+         * 连接broker用，同producer端配置，不用连太多broker，只需要连接其中几个，
+         * 使得某个broker挂掉后仍能连上集群，必输
+         */
         props.put("bootstrap.servers", "192.168.43.152:9092");
-        props.put("group.id", "group");
-        props.put("enable.auto.commit", "true");
+        /**
+         * 消费组名称必输，一般指定一个有业务含义的名称
+         */
+        props.put("group.id", "accountB");
+        props.put("enable.auto.commit", "false");
         props.put("auto.commit.interval.ms", "1000");
         props.put("session.timeout.ms", "30000");
-        props.put("max.poll.records", 1000);
+        props.put("max.poll.interval.ms", "5000");
+        props.put("max.poll.records", 10);
         props.put("auto.offset.reset", "earliest");
+        /**
+         * 解序列化，使用的类要与producer端使用的相对应，必输
+         */
         props.put("key.deserializer", StringDeserializer.class.getName());
+        //同上 必输
         props.put("value.deserializer", StringDeserializer.class.getName());
 
         return  props;
@@ -68,12 +82,27 @@ public class KafkaUtils {
     public static  Properties getProducerProperties(){
         //属性
         Properties props = new Properties();
-        //
+        /**
+         *  参数说明： props.put("bootstrap.servers", "192.168.43.152:9092,192.168.43.152:9093");
+         *  该参数指定连接的broker，如果集群中broker很多的话，只需要指定部分即可，指定多个的原因是因为怕其中一个挂了之后
+         *  producer客户端还能通过其他的ip:port找到集群
+         * 必输
+         */
         props.put("bootstrap.servers", "192.168.43.152:9092");
         props.put("retries", 0);
-        props.put("batch.size", 16384);
+       // props.put("batch.size", 16384);
+        /**
+         * 参数是为消息做序列化用的，任何消息都必须是字节数组才能发送到broker上，
+         * 将一个String字符串转成字节数组
+         * 必输
+         */
         props.put("key.serializer", StringSerializer.class.getName());
+        /**
+         * 必输
+         */
         props.put("value.serializer", StringSerializer.class.getName());
+      //  props.put(ProducerConfig.BUFFER_MEMORY_CONFIG, 106);
+        props.put(ProducerConfig.MAX_BLOCK_MS_CONFIG, 100000);
         return  props;
     }
 }
